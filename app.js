513a07a5ffaf1c11b340cafa0a32244bf700b7a8
quoctrang12 <waipayttt@gmail.com>
2022-09-17 18:28:46 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts',contactsRouter);
+app.use((req,res,next)=>{
+    return next(new ApiError(404, "Resource not found"));
+})
+
+app.use((err,req,res,next)=>{
+    return res.status(err.statusCode||500).json({message: err.message||"internal server Error"})
+})
+
 
 module.exports = app;
